b56b750e6510f9a749ff91071e10630f0b3a1542
Niyaz Razak <niyasibnurazak@gmail.com>
2022-11-06 09:31:05 +0530

    feat: next story

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,45 @@ class _StoryPageState extends State<StoryPage> {
                   ),
                 ),
               ),
+              Expanded(
+                flex: 2,
+                child: MaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      storyBrain.nextStory(1);
+                    });
+                  },
+                  color: Colors.red,
+                  child: Text(
+                    storyBrain.getChoice1(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
+                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
+                child: MaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      storyBrain.nextStory(2);
+                    });
+                  },
+                  color: Colors.blue,
+                  child: Text(
+                    storyBrain.getChoice2(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -3,7 +3,6 @@
 import 'story.dart';
 
 class StoryBrain {
-  int _story_num = 0;
   // ignore: prefer_final_fields
   List<Story> _storyData = [
     Story(
@@ -38,6 +37,40 @@ class StoryBrain {
   ];
 
   String getStory() {
-    return _storyData[_story_num].storyTitle;
+    return _storyData[_storyNumber].storyTitle;
+  }
+
+  String getChoice1() {
+    return _storyData[_storyNumber].choice1;
+  }
+
+  String getChoice2() {
+    return _storyData[_storyNumber].choice2;
+  }
+
+  int _storyNumber = 0;
+
+  void nextStory(int choiceNumber) {
+    if (choiceNumber == 1 && _storyNumber == 0) {
+      _storyNumber = 2;
+    } else if (choiceNumber == 2 && _storyNumber == 0) {
+      _storyNumber = 1;
+    } else if (choiceNumber == 1 && _storyNumber == 1) {
+      _storyNumber = 2;
+    } else if (choiceNumber == 2 && _storyNumber == 1) {
+      _storyNumber = 3;
+    } else if (choiceNumber == 1 && _storyNumber == 2) {
+      _storyNumber = 5;
+    } else if (choiceNumber == 2 && _storyNumber == 2) {
+      _storyNumber = 4;
+    } else if (choiceNumber == 2 && _storyNumber == 2) {
+      _storyNumber = 4;
+    } else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
+      restart();
+    }
+  }
+
+  void restart() {
+    _storyNumber = 0;
   }
 }
