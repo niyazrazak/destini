8bb35855f4c5fcb80d48828785e35da7566a64e3
Niyaz Razak <niyasibnurazak@gmail.com>
2022-11-06 09:41:18 +0530

    feat: reset btn

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -73,4 +73,12 @@ class StoryBrain {
   void restart() {
     _storyNumber = 0;
   }
+
+  bool buttonShouldBeVisible() {
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
